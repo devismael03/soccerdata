fcd6c7178d6fd1cbba31249f8860ff6bedb05215
Ismayil Mammadzada <mamedzade030@gmail.com>
2019-06-12 17:05:36 +0400

    Teams table created

---
+++ database/migrations/2019_06_12_130143_create_teams_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTeamsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('teams', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('team_name');
+            $table->string('team_city');
+            $table->string('team_stadium');
+            $table->string('team_coach');
+            $table->string('team_logo');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('teams');
+    }
+}
