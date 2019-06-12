dc37926d8ec6ed84924379775aaa6de033799724
Ismayil Mammadzada <mamedzade030@gmail.com>
2019-06-12 17:36:14 +0400

    Created Players table

---
+++ database/migrations/2019_06_12_132124_create_players_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePlayersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('players', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('player_name');
+            $table->unsignedInteger('player_number');
+            $table->unsignedBigInteger('team_id')->index();
+            $table->string('player_photo');
+            $table->foreign('team_id')->references('id')->on('teams');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('players');
+    }
+}
