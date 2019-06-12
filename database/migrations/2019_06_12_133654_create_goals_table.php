97abad6f1a2985d8ef480f44e05d9502a65c7ac1
Ismayil Mammadzada <mamedzade030@gmail.com>
2019-06-12 17:37:59 +0400

    Created goals table

---
+++ database/migrations/2019_06_12_133654_create_goals_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateGoalsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('goals', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('match_id')->index();
+            $table->unsignedBigInteger('player_id')->index();
+            $table->unsignedBigInteger('team_id')->index();
+            $table->unsignedInteger('scored_at');
+            $table->foreign('match_id')->references('id')->on('matches');
+            $table->foreign('player_id')->references('id')->on('players');
+            $table->foreign('team_id')->references('id')->on('teams');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('goals');
+    }
+}
