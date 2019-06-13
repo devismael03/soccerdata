f73d146ee90503c4d1f0eab1c068fda49e34e36a
Ismayil Mammadzada <mamedzade030@gmail.com>
2019-06-13 20:59:12 +0400

    Partial views and layouts

---
+++ resources/views/layout/partials/footer.blade.php
@@ -0,0 +1,5 @@
+<footer class="page-footer font-small">
+    <div class="footer-copyright text-center py-3">© 2019 Copyright:
+      <a href="https://www.linkedin.com/in/ismayil-mammadzada-3a2775a6/"> Ismayil Mammadzada</a>
+    </div>
+</footer>
